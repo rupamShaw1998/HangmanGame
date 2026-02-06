8d977b6eadaa8ffd828347fadaa8ba69447b3a58
rupamShaw1998 <rupamshw77777@gmail.com>
2026-02-07 00:40:37 +0530

    drawing comp ui done

--- src/components/Drawing.jsx
+++ src/components/Drawing.jsx
@@ -1,11 +1,38 @@
+const HEAD = (
+  <div className="head" />
+);
+
+const BODY = (
+  <div className="body" />
+);
+
+const RIGHT_ARM = (
+  <div className="right-arm" />
+);
+
+const LEFT_ARM = (
+  <div className="left-arm" />
+);
+
+const RIGHT_LEG = (
+    <div className="right-leg" />
+  );
+  
+  const LEFT_LEG = (
+    <div className="left-leg" />
+  );
+
+const BODY_PARTS = [HEAD, BODY, LEFT_ARM, RIGHT_ARM, LEFT_LEG, RIGHT_LEG];
+
 export function Drawing() {
 
   return (
     <div className="drawing">
-      <div className="rope"></div>
-      <div className="hanger"></div>
-      <div className="pole"></div>
-      <div className="floor"></div>
+      {BODY_PARTS}
+      <div className="rope" />
+      <div className="hanger" />
+      <div className="pole" />
+      <div className="floor" />
     </div>
   )
 }
